b1a3f793fa7aee7567f2ec95d80099a493318c54
Maxim Cherkasov <cherkasov.maxi@gmail.com>
2022-12-02 21:37:49 +0300

    C#_sem2_Остаток от деления 2-х случайных чисел

---
+++ C#/C#_sem_2/C#_sem_2_task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿\// Остаток от деления случайных чисел
+Random rnd = new Random(); //генератор случ. чисел
+int number1 = rnd.Next(10, 100); // случ. число от 10 до 100
+Console.WriteLine(number1);
+int number2 = rnd.Next(10, 100); // случ. число от 10 до 100
+Console.WriteLine(number2);
+int result = (number1%number2);
+if (result==0)
+{
+ Console.WriteLine(number1);
+ Console.WriteLine("Первое число кратно второму");
+ Console.WriteLine(number2);  
+}
+else
+Console.WriteLine("Остаток от деления");
+Console.WriteLine(result);
