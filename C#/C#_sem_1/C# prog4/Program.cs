6d0dea7ac6695fd426f6bbc3613885bd6c1b7c52
Maxim Cherkasov <cherkasov.maxi@gmail.com>
2022-12-03 18:50:52 +0300

    Систематизировал папки еще раз  03.12.22

--- C#/C#_sem_1/C# prog4/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Введите 3-х значное число и получите последнюю цифру этого числа");
-
-int i = int.Parse(Console.ReadLine());
-
-if (i>99 && i < 1000)
-{
-    Console.WriteLine(i%10);
-}
