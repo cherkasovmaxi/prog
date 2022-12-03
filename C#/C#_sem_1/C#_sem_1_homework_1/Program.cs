857aea3a8ca94cb402a86f127e435475321d1c43
Maxim Cherkasov <cherkasov.maxi@gmail.com>
2022-12-03 13:28:35 +0300

    Сделал дз семинара 1 по C#

--- C#/C#_sem_1/C#_sem_1_homework_1/Program.cs
+++ C#/C#_sem_1/C#_sem_1_homework_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Определение наибольшего из двух чисел");
+Console.WriteLine("Введите первое число и нажмите Enter");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число и нажмите Enter");
+int b = int.Parse(Console.ReadLine());
+if (a<b)
+{
+    Console.WriteLine("Второе число больше первого");
+}
+else
+{
+    Console.WriteLine("Первое число больше первого");
+}
